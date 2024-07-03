215a8fc4adde1f472fa65d33120a577baa99a825
Olivier Lando <olivier@api.video>
2024-07-03 10:33:11 +0200

    Functionnal component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
-import { StyleProp, ViewStyle } from 'react-native';
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  type Ref,
+} from 'react';
+import { type StyleProp, type ViewStyle } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const PLAYER_HOST = 'https://embed.api.video';
-const DEFAULT_STYLE = { width: '100%', height: '100%' };
+const DEFAULT_STYLE: StyleProp<ViewStyle> = { width: '100%', height: '100%' };
 
 export type PlayerProps = {
   videoId: string;
@@ -32,6 +38,10 @@ export type PlayerProps = {
   onResize?: () => void;
   onSeeking?: () => void;
   onTimeUpdate?: (currentTime: number) => void;
+  onDurationChange?: (duration: number) => void;
+  onVideoSizeRatioChange?: (ratio: number) => void;
+  onRatioChange?: (ratio: number) => void;
+
   onUserActive?: () => void;
   onUserInactive?: () => void;
   onVolumeChange?: (volume: number) => void;
@@ -44,238 +54,232 @@ const FRAGMENTS = {
   loop: 'loop',
 };
 
-export default class ApiVideoPlayer extends Component<PlayerProps, {}> {
-  webref?: WebView;
-  playerUrl: string;
+export interface ApiVideoPlayerRef {
+  play: () => void;
+  pause: () => void;
+  requestFullscreen: () => void;
+  mute: () => void;
+  unmute: () => void;
+  hideTitle: () => void;
+  showTitle: () => void;
+  hideControls: () => void;
+  showControls: () => void;
+  loadConfigFromUrl: (url: string) => void;
+  seek: (time: number) => void;
+  setCurrentTime: (time: number) => void;
+  setLoop: (loop: boolean) => void;
+  setPlaybackRate: (rate: number) => void;
+  setVolume: (volume: number) => void;
+}
 
-  play() {
-    this.webref?.injectJavaScript(`player.play(); true;`);
-  }
-  pause() {
-    this.webref?.injectJavaScript(`player.pause(); true;`);
-  }
-  requestFullscreen() {
-    this.webref?.injectJavaScript(`player.requestFullscreen(); true;`);
-  }
-  mute() {
-    this.webref?.injectJavaScript(`player.muted(true); true;`);
-  }
-  unmute() {
-    this.webref?.injectJavaScript(`player.muted(false); true;`);
-  }
-  hideTitle() {
-    this.webref?.injectJavaScript(`player.setTitleVisibility(false); true;`);
-  }
-  showTitle() {
-    this.webref?.injectJavaScript(`player.setTitleVisibility(true); true;`);
-  }
-  hideControls() {
-    this.webref?.injectJavaScript(
-      `apiVideoPlayer.setControlsVisibility(false); true;`
-    );
-  }
-  showControls() {
-    this.webref?.injectJavaScript(
-      `apiVideoPlayer.setControlsVisibility(true); true;`
-    );
-  }
-  loadConfigFromUrl(url: string) {
-    this.webref?.injectJavaScript(
-      `apiVideoPlayer.loadConfigFromUrl('${url}'); true;`
-    );
-  }
-  seek(time: number) {
-    if (isNaN(time)) throw new Error('Invalid time');
+const ApiVideoPlayer = forwardRef(
+  (props: PlayerProps, ref: Ref<ApiVideoPlayerRef>) => {
+    const webref = useRef<WebView>(null);
+    const [playerUrl, setPlayerUrl] = React.useState<string>('');
+    const [ratio, setRatio] = React.useState<number>();
 
-    this.webref?.injectJavaScript(
-      `player.currentTime(player.currentTime() + ${time}); true;`
-    );
-  }
-  setCurrentTime(time: number) {
-    if (isNaN(time)) throw new Error('Invalid time');
-
-    this.webref?.injectJavaScript(`player.currentTime(${time}); true;`);
-  }
-  setLoop(loop: boolean) {
-    this.webref?.injectJavaScript(`player.loop(${!!loop}); true;`);
-  }
-  setPlaybackRate(rate: number) {
-    if (isNaN(rate)) throw new Error('Invalid rate');
-
-    this.webref?.injectJavaScript(`player.playbackRate(${rate}); true;`);
-  }
-  setVolume(volume: number) {
-    if (isNaN(volume)) throw new Error('Invalid volume');
-
-    this.webref?.injectJavaScript(`player.volume(${volume}); true;`);
-  }
-
-  constructor(props: PlayerProps) {
-    super(props);
-    this.playerUrl = this.buildEmbedUrl(props);
-  }
-
-  componentDidUpdate(prevProps: PlayerProps) {
-    if (
-      typeof this.props.hideControls !== 'undefined' &&
-      prevProps.hideControls !== this.props.hideControls
-    ) {
-      this.props.hideControls ? this.hideControls() : this.showControls();
-    }
-    if (
-      typeof this.props.loop !== 'undefined' &&
-      prevProps.loop !== this.props.loop
-    ) {
-      this.setLoop(this.props.loop);
-    }
-    if (
-      typeof this.props.muted !== 'undefined' &&
-      prevProps.muted !== this.props.muted
-    ) {
-      this.props.muted ? this.mute() : this.unmute();
-    }
-    if (
-      typeof this.props.hideTitle !== 'undefined' &&
-      prevProps.hideTitle !== this.props.hideTitle
-    ) {
-      this.props.hideTitle ? this.hideTitle() : this.showTitle();
-    }
-    if (
-      typeof this.props.videoId !== 'undefined' &&
-      prevProps.videoId !== this.props.videoId
-    ) {
-      this.loadConfigFromUrl(this.buildEmbedUrl(this.props));
-    }
-    if (
-      typeof this.props.type !== 'undefined' &&
-      prevProps.type !== this.props.type
-    ) {
-      this.loadConfigFromUrl(this.buildEmbedUrl(this.props));
-    }
-  }
-
-  private onMessage(message: any) {
-    if (!message.type) {
-      return;
-    }
-    switch (message.type) {
-      case 'controlsdisabled':
-        if (this.props.onControlsDisabled) this.props.onControlsDisabled();
-        break;
-      case 'controlsenabled':
-        if (this.props.onControlsEnabled) this.props.onControlsEnabled();
-        break;
-      case 'ended':
-        if (this.props.onEnded) this.props.onEnded();
-        break;
-      case 'error':
-        if (this.props.onError) this.props.onError();
-        break;
-      case 'firstplay':
-        if (this.props.onFirstPlay) this.props.onFirstPlay();
-        break;
-      case 'fullscreenchange':
-        if (this.props.onFullScreenChange) this.props.onFullScreenChange();
-        break;
-      case 'pause':
-        if (this.props.onPause) this.props.onPause();
-        break;
-      case 'play':
-        if (this.props.onPlay) this.props.onPlay();
-        break;
-      case 'playerresize':
-        if (this.props.onPlayerResize) this.props.onPlayerResize();
-        break;
-      case 'qualitychange':
-        if (this.props.onQualityChange)
-          this.props.onQualityChange(message.resolution);
-        break;
-      case 'ratechange':
-        if (this.props.onRateChange) this.props.onRateChange();
-        break;
-      case 'ready':
-        if (this.props.onReady) this.props.onReady();
-        break;
-      case 'resize':
-        if (this.props.onResize) this.props.onResize();
-        break;
-      case 'seeking':
-        if (this.props.onSeeking) this.props.onSeeking();
-        break;
-      case 'timeupdate':
-        if (this.props.onTimeUpdate)
-          this.props.onTimeUpdate(message.currentTime);
-        break;
-      case 'useractive':
-        if (this.props.onUserActive) this.props.onUserActive();
-        break;
-      case 'userinactive':
-        if (this.props.onUserInactive) this.props.onUserInactive();
-        break;
-      case 'volumechange':
-        if (this.props.onVolumeChange)
-          this.props.onVolumeChange(message.volume);
-        break;
-    }
-  }
-
-  buildQueryParameters(originalProps: any): string {
-    if (
-      'privateToken' in originalProps ||
-      'sessionToken' in originalProps ||
-      ('muted' in originalProps && originalProps.muted)
-    ) {
-      let query = '?';
-      const keyToQueryParameMap: any = {
-        privateToken: 'token',
-        sessionToken: 'avh',
-        muted: 'muted',
-      };
-      for (const key in originalProps) {
-        if (
-          key === 'privateToken' ||
-          key === 'sessionToken' ||
-          key === 'muted'
-        ) {
-          query += `${keyToQueryParameMap[key]}=${originalProps[key]}&`;
+    const buildQueryParameters = (originalProps: any): string => {
+      if (
+        'privateToken' in originalProps ||
+        'sessionToken' in originalProps ||
+        ('muted' in originalProps && originalProps.muted)
+      ) {
+        let query = '?';
+        const keyToQueryParameMap: any = {
+          privateToken: 'token',
+          sessionToken: 'avh',
+          muted: 'muted',
+        };
+        for (const key in originalProps) {
+          if (
+            key === 'privateToken' ||
+            key === 'sessionToken' ||
+            key === 'muted'
+          ) {
+            query += `${keyToQueryParameMap[key]}=${originalProps[key]}&`;
+          }
         }
+        return query;
+      } else {
+        return '';
       }
-      return query;
-    } else {
-      return '';
-    }
-  }
+    };
 
-  buildEmbedUrl(props: PlayerProps) {
-    let url = `${PLAYER_HOST}/${this.props.type || 'vod'}/${props.videoId}`;
+    useEffect(() => {
+      setPlayerUrl(buildEmbedUrl(props));
+    }, [props.videoId, props.type]);
 
-    url += this.buildQueryParameters(props);
+    const buildEmbedUrl = (props: PlayerProps) => {
+      let url = `${PLAYER_HOST}/${props.type || 'vod'}/${props.videoId}`;
+      url += buildQueryParameters(props);
+      const fragments = Object.keys(FRAGMENTS).filter(
+        (fragment: string) => (props as any)[fragment] === true
+      );
+      if (fragments.length > 0) {
+        url += '#' + fragments.map((f: any) => (FRAGMENTS as any)[f]).join(';');
+      }
+      return url;
+    };
 
-    const fragments = Object.keys(FRAGMENTS).filter(
-      (fragment: string) => (props as any)[fragment] === true
-    );
+    const injectJavaScript = (script: string) => {
+      webref.current?.injectJavaScript(`${script} true;`);
+    };
 
-    if (fragments.length > 0) {
-      url += '#' + fragments.map((f: any) => (FRAGMENTS as any)[f]).join(';');
-    }
-    return url;
-  }
+    useImperativeHandle(ref, () => ({
+      play: () => injectJavaScript('player.play();'),
+      pause: () => injectJavaScript('player.pause();'),
+      requestFullscreen: () => injectJavaScript('player.requestFullscreen();'),
+      mute: () => injectJavaScript('player.muted(true);'),
+      unmute: () => injectJavaScript('player.muted(false);'),
+      hideTitle: () => injectJavaScript('player.setTitleVisibility(false);'),
+      showTitle: () => injectJavaScript('player.setTitleVisibility(true);'),
+      hideControls: () =>
+        injectJavaScript('apiVideoPlayer.setControlsVisibility(false);'),
+      showControls: () =>
+        injectJavaScript('apiVideoPlayer.setControlsVisibility(true);'),
+      loadConfigFromUrl: (url: string) =>
+        injectJavaScript(`apiVideoPlayer.loadConfigFromUrl('${url}');`),
+      seek: (time: number) => {
+        if (isNaN(time)) throw new Error('Invalid time');
+        injectJavaScript(`player.currentTime(player.currentTime() + ${time});`);
+      },
+      setCurrentTime: (time: number) => {
+        if (isNaN(time)) throw new Error('Invalid time');
+        injectJavaScript(`player.currentTime(${time});`);
+      },
+      setLoop: (loop: boolean) => injectJavaScript(`player.loop(${!!loop});`),
+      setPlaybackRate: (rate: number) => {
+        if (isNaN(rate)) throw new Error('Invalid rate');
+        injectJavaScript(`player.playbackRate(${rate});`);
+      },
+      setVolume: (volume: number) => {
+        if (isNaN(volume)) throw new Error('Invalid volume');
+        injectJavaScript(`player.volume(${volume});`);
+      },
+    }));
 
-  render() {
-    return (
+    useEffect(() => {
+      if (props.hideControls !== undefined) {
+        props.hideControls
+          ? injectJavaScript('apiVideoPlayer.setControlsVisibility(false);')
+          : injectJavaScript('apiVideoPlayer.setControlsVisibility(true);');
+      }
+    }, [props.hideControls]);
+
+    useEffect(() => {
+      if (props.loop !== undefined) {
+        injectJavaScript(`player.loop(${props.loop});`);
+      }
+    }, [props.loop]);
+
+    useEffect(() => {
+      if (props.muted !== undefined) {
+        props.muted
+          ? injectJavaScript('player.muted(true);')
+          : injectJavaScript('player.muted(false);');
+      }
+    }, [props.muted]);
+
+    useEffect(() => {
+      if (props.hideTitle !== undefined) {
+        props.hideTitle
+          ? injectJavaScript('player.setTitleVisibility(false);')
+          : injectJavaScript('player.setTitleVisibility(true);');
+      }
+    }, [props.hideTitle]);
+
+    useEffect(() => {
+      if (props.videoId !== undefined || props.type !== undefined) {
+        injectJavaScript(
+          `apiVideoPlayer.loadConfigFromUrl('${buildEmbedUrl(props)}');`
+        );
+      }
+    }, [props.videoId, props.type]);
+
+    const onMessage = (message: any) => {
+      if (!message.type) return;
+      switch (message.type) {
+        case 'controlsdisabled':
+          props.onControlsDisabled && props.onControlsDisabled();
+          break;
+        case 'controlsenabled':
+          props.onControlsEnabled && props.onControlsEnabled();
+          break;
+        case 'ended':
+          props.onEnded && props.onEnded();
+          break;
+        case 'error':
+          props.onError && props.onError();
+          break;
+        case 'firstplay':
+          props.onFirstPlay && props.onFirstPlay();
+          break;
+        case 'fullscreenchange':
+          props.onFullScreenChange && props.onFullScreenChange();
+          break;
+        case 'pause':
+          props.onPause && props.onPause();
+          break;
+        case 'play':
+          props.onPlay && props.onPlay();
+          break;
+        case 'playerresize':
+          props.onPlayerResize && props.onPlayerResize();
+          break;
+        case 'qualitychange':
+          if (message.resolution.height) {
+            const newRatio =
+              message.resolution.width / message.resolution.height;
+            if (newRatio !== ratio) {
+              setRatio(newRatio);
+              props.onRatioChange && props.onRatioChange(newRatio);
+            }
+          }
+          props.onQualityChange && props.onQualityChange(message.resolution);
+          break;
+        case 'ratechange':
+          props.onRateChange && props.onRateChange();
+          break;
+        case 'ready':
+          props.onReady && props.onReady();
+          break;
+        case 'resize':
+          props.onResize && props.onResize();
+          break;
+        case 'seeking':
+          props.onSeeking && props.onSeeking();
+          break;
+        case 'timeupdate':
+          props.onTimeUpdate && props.onTimeUpdate(message.currentTime);
+          break;
+        case 'useractive':
+          props.onUserActive && props.onUserActive();
+          break;
+        case 'userinactive':
+          props.onUserInactive && props.onUserInactive();
+          break;
+        case 'volumechange':
+          props.onVolumeChange && props.onVolumeChange(message.volume);
+          break;
+        default:
+          break;
+      }
+    };
+
+    return playerUrl ? (
       <WebView
-        ref={(r: any) => (this.webref = r)}
-        source={{ uri: this.playerUrl }}
-        style={this.props.style || DEFAULT_STYLE}
+        ref={webref}
+        source={{ uri: playerUrl }}
+        style={props.style || DEFAULT_STYLE}
         scrollEnabled={false}
-        onMessage={(msg: any) =>
-          this.onMessage(JSON.parse(msg.nativeEvent.data))
-        }
+        onMessage={(msg) => onMessage(JSON.parse(msg.nativeEvent.data))}
         allowsInlineMediaPlayback={true}
         allowsFullscreenVideo={true}
         mediaPlaybackRequiresUserAction={false}
         injectedJavaScriptBeforeContentLoaded={`window.addEventListener('message', (m) => window.ReactNativeWebView.postMessage(JSON.stringify(m.data)))`}
       />
-    );
+    ) : null;
   }
-}
+);
+
+export default ApiVideoPlayer;
